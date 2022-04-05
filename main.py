b79708bd270cb0954401f3b52371b9cbe49c37e2
euno <seal5945@gmail.com>
2022-04-05 12:58:46 +0900

    이미지 제거 오류 수정중

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 import sys
 import os
+import send2trash
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        widget = QWidget()
+        self.my_layout = QGridLayout(widget)
+        self.setCentralWidget(widget)
+        self.setLayout(self.my_layout)
         self.img_index = 0
-        self.lbl_img = QLabel()
+        self.lbl_img = QLabel(self)
         self.img_list = ['']
         self.initUI()
 
     def initUI(self):
+        self.setGeometry(100, 100, 1200, 800)
         file_name = ''
         pixmap = QPixmap(file_name)
         self.lbl_img.setPixmap(pixmap)
-        self.lbl_img.move(0, 0)
         self.setMenu()
         self.setButton()
         self.setWindowTitle('Dataset Filter')
-        self.setGeometry(100, 100, 1200, 800)
+        self.my_layout.addWidget(self.lbl_img, 1, 1)
         self.show()
 
     def setButton(self):
         prev_btn = QPushButton('Prev', self)
         prev_btn.resize(prev_btn.sizeHint())
-        prev_btn.move(20, 400)
+        prev_btn.clicked.connect(self.btnPrev_clicked)
+        self.my_layout.addWidget(prev_btn, 2, 0)
+
+        del_btn = QPushButton('Del', self)
+        del_btn.resize(del_btn.sizeHint())
+        del_btn.clicked.connect(self.btnDel_clicked)
+        self.my_layout.addWidget(del_btn, 2, 1)
+
         next_btn = QPushButton('Next', self)
         next_btn.resize(next_btn.sizeHint())
         next_btn.clicked.connect(self.btnNext_clicked)
-        next_btn.move(1100, 400)
+        self.my_layout.addWidget(next_btn, 2, 2)
 
     def setMenu(self):
         menubar = self.menuBar()
@@ -43,17 +54,19 @@ class MyApp(QMainWindow):
         filemenu = menubar.addMenu('&File')
         filemenu.addAction(selectFolder)
         filemenu.addAction(exitAction)
+        # self.my_layout.addWidget(filemenu, 0, 0)
 
     def getFileList(self):
         fname = QFileDialog.getExistingDirectory(self, "Select Directory")
         file_list = os.listdir(fname)
-        file_list_jpg = [os.path.join(fname, file) for file in file_list if file.endswith(".jpg") or file.endswith(".png")]
+        file_list_jpg = [os.path.join(fname, file) for file in file_list if
+                         file.endswith(".jpg") or file.endswith(".png")]
         print(file_list_jpg)
         self.img_list = file_list_jpg
         pixmap = QPixmap(self.img_list[0])
-        print(self.img_list[0])
+        pixmap.scaledToWidth(800)
+        pixmap.scaledToHeight(800)
         self.lbl_img.setPixmap(pixmap)
-        self.lbl_img.move(0, 0)
         self.repaint()
 
     def btnNext_clicked(self):
@@ -62,17 +75,38 @@ class MyApp(QMainWindow):
         self.lbl_img.setPixmap(pixmap)
         self.repaint()
 
+    def btnDel_clicked(self):
+        head, tail = self.img_list[self.img_index].rsplit("\\", 1)
+        label_dir = os.path.join(head, "label")
+        img_dir = os.path.join(head, "image")
+        label_file, _ = tail.split(".")
+        label_file = label_file + ".txt"
+        label_file = os.path.join(label_dir, label_file)
+        img_file, kind = tail.split(".")
+        if kind == "jpg":
+            img_file = img_file + "jpg"
+        elif kind == "png":
+            img_file = img_file + "png"
+        else:
+            print("지정하지 않은 포멧")
+
+        img_file = os.path.join(img_dir, img_file)
+        print(img_file)
+        pixmap = QPixmap(self.img_list[self.img_index + 1])
+        self.lbl_img.setPixmap(pixmap)
+        print(self.img_list[self.img_index])
+        send2trash.send2trash(self.img_list.pop(self.img_list[self.img_index]))
+        print("레이블링 이미지 제거 성공")
+        send2trash.send2trash(label_file)
+        print("레이블 제거 성공")
+        send2trash.send2trash(img_file)
+        print("이미지 제거 성공")
+
     def btnPrev_clicked(self):
         self.img_index -= 1
         pixmap = QPixmap(self.img_list[self.img_index])
         self.lbl_img.setPixmap(pixmap)
         self.repaint()
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
 
 
 if __name__ == '__main__':
